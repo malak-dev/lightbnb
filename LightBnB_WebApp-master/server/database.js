fbf3dc4cad5ed0088103e24e851cba8dc515aa64
malak <malak-y@live.com>
2020-01-09 21:02:12 +0000

    update properties table

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -136,9 +136,12 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function (property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  const { owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms } = property;
+  const queryString = `INSERT INTO properties(owner_id , title ,description ,thumbnail_photo_url, cover_photo_url, cost_per_night, street ,city , province ,post_code ,country ,parking_spaces , number_of_bathrooms ,number_of_bedrooms) 
+  VALUES($1 ,$2 ,$3 ,$4 ,$5 ,$6 ,$7 ,$8 ,$9 ,$10 ,$11, $12 ,$13 ,$14) RETURNING *`;
+  const values = [owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms]
+  return pool.query(queryString, values)
+    .then(res => res.rows)
 }
+
 exports.addProperty = addProperty;
